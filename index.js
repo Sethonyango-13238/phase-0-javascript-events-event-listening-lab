eb33065846ce96d497f1112077248e171d611c19
Sethonyango-13238 <onyangosethoyugi@gmail.com>
2024-07-08 18:36:53 +0300

    event

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+function clickAllert(){
+    alert('I was clicked!');
 }
+input.addEventListener('click',clickAllert);
+}  
+addingEventListener();
+
+
+
+// const input = document.getElementById('button');
+// input.addEventListener('click', function(){
+//     alert('I was clicked!');
+// });
+
+// const input = document.getElementById('button');
+// function clickAllert(){
+//     alert('I was clicked!');
+// }
+// input.addEventListener('click',clickAllert);
